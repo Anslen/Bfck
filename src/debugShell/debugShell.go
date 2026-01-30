f4294ec1882f19312b5e00107cab75cb3b4b096b
Anslen <Anslensh@gmail.com>
2026-01-30 20:29:15 +0800

    Add tape command

--- src/debugShell/debugShell.go
+++ src/debugShell/debugShell.go
@@ -30,22 +30,25 @@ import (
 const HELP_STRING string = "r[un]                    : Run code from begin\n" +
 	"c[ontinue]               : Continue running code until next breakpoint or end\n" +
 	"s[tep]:				  : Step to next operator \n" +
+	"d[etailed] [times]     : Detailed step for specified times, show each operator and tape after execute, default run until finish\n" +
 	"u[ntil]                  : Run until loop([]) finish\n" +
 	"w[atch] <offset>         : Watch memory byte at current pointer plus offset\n" +
 	"b[reak] <line>           : Set breakpoint at specified line\n" +
-	"d[elete] <line>          : Delete breakpoint at specified line\n" +
+	"del[ete] <line>          : Delete breakpoint at specified line\n" +
 	"i[nfo]                   : Information of breakpoints and watching\n" +
 	"clear                    : Clear all breakpoints\n" +
 	"p[eek] [offset [length]] : Peek memory bytes at current pointer with optional offset and length\n" +
+	"t[ape]                   : Show tape around, equal to peek -10 20\n" +
 	"n[ext]                   : Show next operator to be executed\n" +
 	"code                     : Show analysed code information\n" +
 	"h[elp]                   : Show this help message\n" +
 	"q[uit]                   : Quit debug shell\n" +
 	"\n"
 
+var REG_DETAILED *regexp.Regexp = regexp.MustCompile(`^d(etailed)?( (\d+))?$`)
 var REG_WATCH *regexp.Regexp = regexp.MustCompile(`^w(atch)? (-?\d+)$`)
 var REG_BREAK *regexp.Regexp = regexp.MustCompile(`^b(reak)? (\d+)$`)
-var REG_DELETE *regexp.Regexp = regexp.MustCompile(`^d(elete)? (\d+)$`)
+var REG_DELETE *regexp.Regexp = regexp.MustCompile(`^del(ete)? (\d+)$`)
 var REG_PEEK *regexp.Regexp = regexp.MustCompile(`^p(eek)?( (-?\d+)( (\d+))?)?$`)
 
 // Start starts the debug shell for the given code runner.
@@ -117,12 +120,10 @@ func Start(codeRunner *coderunner.CodeRunner) {
 			default:
 				panic("DebugShell: Unknown return code")
 			}
-			fmt.Print("\n")
 			continue
 
 		case "s", "step":
 			var ret coderunner.ReturnCode = codeRunner.Step()
-			fmt.Print("\n")
 
 			// Check return code
 			if ret == coderunner.ReturnAfterFinish {
@@ -130,6 +131,10 @@ func Start(codeRunner *coderunner.CodeRunner) {
 			} else {
 				CodeRunning = true
 			}
+			continue
+
+		case "t", "tape":
+			peekTape(codeRunner, -10, 20)
 			continue
 
 		case "u", "until":
@@ -164,6 +169,30 @@ func Start(codeRunner *coderunner.CodeRunner) {
 
 		case "q", "quit":
 			return
+		}
+
+		if matches := REG_DETAILED.FindStringSubmatch(command); matches != nil {
+			// regex match detailed command
+			var times uint64
+			if matches[3] == "" {
+				times = ^uint64(0)
+			} else {
+				fmt.Sscanf(matches[3], "%d", &times)
+			}
+
+			var i uint64
+			for i = 0; i < times; i++ {
+				var ret coderunner.ReturnCode = detailedStep(codeRunner)
+				// Check return code
+				if ret == coderunner.ReturnAfterFinish {
+					fmt.Print("\n\nRunning finished\n\n")
+					CodeRunning = false
+					break
+				} else {
+					CodeRunning = true
+				}
+			}
+			continue
 		}
 
 		if matches := REG_WATCH.FindStringSubmatch(command); matches != nil {
@@ -214,20 +243,35 @@ func Start(codeRunner *coderunner.CodeRunner) {
 				fmt.Sscanf(matches[5], "%d", &length)
 			}
 
-			var bytes []byte = codeRunner.PeekBytes(offset, length)
-			// Print bytes
-			for index, each := range bytes {
-				if offset+index == 0 {
-					fmt.Printf("[%d] ", each)
-				} else {
-					fmt.Printf("%d ", each)
-				}
-			}
-			fmt.Print("\n\n")
+			peekTape(codeRunner, offset, length)
 			continue
 		}
 
 		// No match command, print help
 		fmt.Print("Unknown command. Type h for help\n\n")
 	}
+}
+
+// peekTape peeks memory bytes at the given offset and length, and prints them.
+func peekTape(codeRunner *coderunner.CodeRunner, offset, length int) {
+	var bytes []byte = codeRunner.PeekBytes(offset, length)
+	// Print bytes
+	for index, each := range bytes {
+		if offset+index == 0 {
+			fmt.Printf("[%d] ", each)
+		} else {
+			fmt.Printf("%d ", each)
+		}
+	}
+	fmt.Print("\n\n")
+}
+
+// detailedStep performs a single step and prints detailed information.
+func detailedStep(codeRunner *coderunner.CodeRunner) (ret coderunner.ReturnCode) {
+	codeRunner.PrintNextOperator()
+	fmt.Print("\n")
+	ret = codeRunner.Step()
+	// Print tape around
+	peekTape(codeRunner, -10, 20)
+	return
 }
